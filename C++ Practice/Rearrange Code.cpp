c4da7dc7b9597b8395ce5b19332ea52e52dc6f49
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2024-03-01 07:07:38 +0530

    Update Rearrange Code.cpp

--- C++ Practice/Rearrange Code.cpp
+++ C++ Practice/Rearrange Code.cpp
@@ -8,3 +8,14 @@ int main() {
   cout << sum;
   return 0;
 }
+
+//part2
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  for(int i = 1; i <= 10; i++)
+  {
+    cout << i << endl;
+  }
+}
