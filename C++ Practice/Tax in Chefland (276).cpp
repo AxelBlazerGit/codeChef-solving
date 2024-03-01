8a55cc02542531a0b422c8d8056a65f99ec5d595
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2024-03-02 01:53:13 +0530

    Create Tax in Chefland (276).cpp

---
+++ C++ Practice/Tax in Chefland (276).cpp
@@ -0,0 +1,18 @@
+// Update the code below to solve the problem
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+ int t;
+ cin>>t;
+ while(t--)
+  { 
+   int x;
+   //Accept 1 integer inputs.
+   cin>>x;       
+   x>100?cout<<x-10<<endl:cout<<x<<endl;
+  }
+ return 0;
+}
