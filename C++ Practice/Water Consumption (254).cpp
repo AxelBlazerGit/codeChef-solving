ca42295950417dfc20050938f256bb7127350fb0
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2024-03-01 07:14:15 +0530

    Create Water Consumption (254).cpp

---
+++ C++ Practice/Water Consumption (254).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        // your code goes here
+        x>1999?cout<<"yes"<<endl:cout<<"no"<<endl;
+    }
+	return 0;
+}
