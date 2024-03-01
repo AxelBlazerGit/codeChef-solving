36b4334d52872606e7ef4d675d4fbbd68ac20f9c
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2024-03-01 07:12:28 +0530

    Create Good Turn (238).cpp

---
+++ C++ Practice/Good Turn (238).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        // your code goes here
+        x+y>6?cout<<"YES"<<endl:cout<<"NO"<<endl;
+    }
+	return 0;
+}
