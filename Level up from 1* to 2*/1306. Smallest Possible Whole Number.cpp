ca00dfb6c4aa4cbda12dcc00fc75e754fa481656
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2024-03-09 11:06:12 +0530

    Create 1306. Smallest Possible Whole Number.cpp

---
+++ Level up from 1* to 2*/1306. Smallest Possible Whole Number.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,n,k;
+	cin>>t;
+	while(t--){
+	    cin>>n>>k;
+	    if(k!=0) cout<<n%k<<endl;
+	    else cout<<n<<endl;
+	}
+
+}
