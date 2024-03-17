b72b8d046fed2ae7c567a39ab35cb296af8b64f9
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2024-03-18 04:14:31 +0530

    Create 1329.Indivisible Permutation.cpp

---
+++ Jump from 2* to 3*/1329.Indivisible Permutation.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    //6=> 6 1 2 3 4 5
+	// your code goes here
+    int t,n;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        cout<<n<<" ";
+        for(int i=1;i<n;i++) cout<<i<<" ";
+        cout<<endl;
+        //2 1 2 3
+        // arr[0]=2;
+        // for(int i=1;i<n/2;i++) arr[i]=i;
+        // for(int i=n/2;i<n-1;i++) arr[i]=i+2;
+        // arr[n-1]=n/2+1;
+        // for(auto it:arr) cout<<it<<" ";
+        // cout<<endl;
+        // for(int i=0;i<n;i++){
+        //     if(i<=n/2) arr[i]=i+2;
+        //     else arr[i]=i-n/2+1;
+        // }
+        
+        // for(int i=0;i<n;i++){
+        //     cout<<arr[i]<<" ";
+        // }
+        // cout<<endl;
+    }
+    
+}
